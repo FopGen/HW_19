3808f63d4e805b45ecb6ebab6803a12ae4ada227
Evgen <fopgen@gmail.com>
2024-04-08 08:12:54 +0300

    added build.yml

---
+++ src/main/java/com/example/hw_19/SumCalculator.java
@@ -0,0 +1,20 @@
+package com.example.hw_19;
+
+public class SumCalculator{
+    SumCalculator() {
+    }
+
+    static int sum(int userValue) throws Exception{
+
+        if (userValue == 0 ){
+            throw new IllegalArgumentException(userValue + " - incorrect number");
+        }
+        int result = 0;
+        for (int i = 1; i < userValue+1; i++) {
+            result += i;
+        }
+
+        return result;
+    }
+
+}
